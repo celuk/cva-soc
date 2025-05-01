2627efc8fb0deb1004dc2a58294f56e3d5eb32b8
celuk <seyyid4091@gmail.com>
2025-05-01 21:42:26 +0300

    use hpdcache wt

--- verification/sim/main.py
+++ verification/sim/main.py
@@ -68,7 +68,6 @@ def run_test(simulator: str, test_file: Path, top_module: str, waves: bool, cfil
         and not str(path).rsplit('/', 1)[-1].startswith("Sim")
         and not str(path).rsplit('/', 1)[-1].endswith("riscv.sv")
         and "deprecated" not in str(path)
-        and "hpdcache" not in str(path)
         and not str(path).rsplit('/', 1)[-1].startswith("hpdcache_wrapper.sv")
         and "hpdcache_to_l15" not in str(path)
         and "wt_l15_adapter" not in str(path)
