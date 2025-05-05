74ae662882b603805856502e4ec24901df6040f5
celuk <seyyid4091@gmail.com>
2025-05-05 23:00:24 +0300

    working simple

--- rtl/src/config/custom_config.sv
+++ rtl/src/config/custom_config.sv
@@ -72,7 +72,7 @@ package cva6_config_pkg;
       CachedRegionAddrBase: 1024'({64'h0000_0000}),
       CachedRegionLength: 1024'({64'h0D00_0000}),
       MaxOutstandingStores: unsigned'(0),
-      DebugEn: bit'(0),
+      DebugEn: bit'(1),
       AxiBurstWriteEn: bit'(0),
       IcacheByteSize: unsigned'(2048),
       IcacheSetAssoc: unsigned'(4),
