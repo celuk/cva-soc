10bd67092d7666ee286af1b8a4523f9c7d08dc76
celuk <seyyid4091@gmail.com>
2025-05-26 18:22:30 +0300

    wrong dram demo

--- tests/dram_demo/dram_demo.c
+++ tests/dram_demo/dram_demo.c
@@ -4,6 +4,10 @@
 #include "timer.h"
 #include "defines.h"
 
+#define DDR100 (*(volatile uint32_t*)(DDR_BASE_ADDR + 0x100))
+#define DDR200 (*(volatile uint32_t*)(DDR_BASE_ADDR + 0x100))
+#define DDR300 (*(volatile uint32_t*)(DDR_BASE_ADDR + 0x100))
+
 void write_to_ddr3(unsigned int offset_in_ddr, unsigned int data) {
     volatile unsigned int* ptr = (*(volatile uint32_t*)(DDR_BASE_ADDR + offset_in_ddr));
     *ptr = data;
@@ -18,11 +22,15 @@ int main() {
     init_uart();
     wait_for_us(500);
 
-    write_to_ddr3(0x100, 0x1241BAEF);
-    write_to_ddr3(0x200, 0x1241BEEF);
-    write_to_ddr3(0x300, 0x1242BAEF);
+    //write_to_ddr3(0x100, 0x1241BAEF);
+    //write_to_ddr3(0x200, 0x1241BEEF);
+    //write_to_ddr3(0x300, 0x1242BAEF);
 
-    unsigned int value = read_from_ddr3(0x100);
+    DDR100 = 0x1241BAEF;
+    DDR200 = 0x1241BEEF;
+    DDR300 = 0x1242BAEF;
+
+    unsigned int value = DDR200; //read_from_ddr3(0x100);
 
     tekno_printf("Value read from dram: %x\n", value);
 
