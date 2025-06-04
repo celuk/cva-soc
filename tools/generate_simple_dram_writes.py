de2873575d3e71b54896a4737a213fae650dd178
celuk <seyyid4091@gmail.com>
2025-06-04 13:51:55 +0300

    write Fs in latest chunk data

--- tools/generate_simple_dram_writes.py
+++ tools/generate_simple_dram_writes.py
@@ -96,6 +96,9 @@ def generate_files(hex_file):
 
     for i in range(num_chunks):
         chunk = words[i * WORDS_PER_FILE : (i + 1) * WORDS_PER_FILE]
+        if i == num_chunks - 1:
+            chunk.append('FFFFFFFF')
+        
         folder_name = f"simple_dram_write{i}"
         folder_path = os.path.join(BUILD_DIR, folder_name)
         os.makedirs(folder_path, exist_ok=True)
