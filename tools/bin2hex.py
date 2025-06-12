f4765879a41a13289a804ed47f7a4b0b0453b13e
celuk <seyyid4091@gmail.com>
2025-06-12 23:32:21 +0300

    try different start assembly

--- tools/bin2hex.py
+++ tools/bin2hex.py
@@ -1,11 +1,4 @@
 #!/usr/bin/env python3
-#
-# This is free and unencumbered software released into the public domain.
-#
-# Anyone is free to copy, modify, publish, use, compile, sell, or
-# distribute this software, either in source code form or as a compiled
-# binary, for any purpose, commercial or non-commercial, and by any
-# means.
 
 import sys
 
@@ -23,3 +16,17 @@ with open(sys.argv[1], "rb") as f:
             s = ["00"]*4
             cnt = 4
         cnt -= 1
+
+#import sys
+#
+#with open(sys.argv[1], "rb") as f:
+#    binData = f.read()
+#
+#maxlimit = 1000000
+#
+#assert len(binData) < 4 * maxlimit
+#
+#for i in range(maxlimit):
+#    if i < len(binData) // 4:
+#        w = binData[4 * i : 4 * i + 4]
+#        print("%02x%02x%02x%02x" % (w[3], w[2], w[1], w[0]))
