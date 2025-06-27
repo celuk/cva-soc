523bd0513188680a49d092e61c8a764f1b207a97
celuk <seyyid4091@gmail.com>
2025-06-27 13:30:54 +0300

    able to change base address in dram chunk script

--- tests/simple_dram_write/simple_dram_write.c
+++ tests/simple_dram_write/simple_dram_write.c
@@ -3,6 +3,9 @@ int main()
     unsigned int address = 0x80000000;
     unsigned int offset = 0x12345128;
     unsigned int value = 0xDEADBEEF;
+    //unsigned int address = 0x80000000;
+    //unsigned int offset = 0x2F456234;
+    //unsigned int value = 0xABCD1234;
     *((volatile unsigned int*)(address + offset)) = value;
     
     return 0;
--- tools/dram_write_chunk_hexs.py
+++ tools/dram_write_chunk_hexs.py
@@ -1,5 +1,7 @@
-def modify_hex(hex_code, new_offset, new_value):
+def modify_hex(hex_code, new_base_address, new_offset, new_value):
     instructions = hex_code.split()
+    
+    final_address = new_base_address + new_offset
     
     value_upper = (new_value >> 12) & 0xFFFFF
     if new_value & 0x800:
@@ -8,15 +10,15 @@ def modify_hex(hex_code, new_offset, new_value):
     if value_lower & 0x800:
         value_lower |= 0xFFFFF000
     
-    offset_upper = (new_offset >> 12) & 0xFFFFF
-    if new_offset & 0x800:
-        offset_upper += 1
-    offset_lower = new_offset & 0xFFF
+    addr_upper = (final_address >> 12) & 0xFFFFF
+    if final_address & 0x800:
+        addr_upper += 1
+    addr_lower = final_address & 0xFFF
     
     instructions[0] = f"{(0x37 | (15 << 7) | (value_upper << 12)):08X}"
-    instructions[1] = f"{(0x37 | (14 << 7) | (offset_upper << 12)):08X}"
+    instructions[1] = f"{(0x37 | (14 << 7) | (addr_upper << 12)):08X}"
     instructions[2] = f"{(0x13 | (15 << 7) | (15 << 15) | ((value_lower & 0xFFF) << 20)):08X}"
-    instructions[3] = f"{(0x23 | (2 << 12) | (14 << 15) | (15 << 20) | ((offset_lower & 0x1F) << 7) | (((offset_lower >> 5) & 0x7F) << 25)):08X}"
+    instructions[3] = f"{(0x23 | (2 << 12) | (14 << 15) | (15 << 20) | ((addr_lower & 0x1F) << 7) | (((addr_lower >> 5) & 0x7F) << 25)):08X}"
     
     return '\n'.join(instructions)
 
@@ -28,8 +30,14 @@ EEF78793
 00000513 
 00008067
 """
+
+#old_base_address = 0x80000000
+#old_offset = 0x12345128
+#old_value = 0xDEADBEEF
+
+new_base_address = 0x80000000
 new_offset = 0x2F456234
 new_value = 0xABCD1234
 
-modified_hex = modify_hex(original_hex, new_offset, new_value)
+modified_hex = modify_hex(original_hex, new_base_address, new_offset, new_value)
 print(modified_hex)
