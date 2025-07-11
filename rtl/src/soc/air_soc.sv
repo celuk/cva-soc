1496dde98664988c19d92b639b1dde39a54b356b
celuk <seyyid4091@gmail.com>
2025-07-11 14:19:22 +0300

    working opensbi with sram

--- rtl/src/soc/air_soc.sv
+++ rtl/src/soc/air_soc.sv
@@ -827,7 +827,7 @@ module air_soc (
 
    ram32 #(
       .SIZE     ('h40000/4),
-      .INIT_FILE(`RAM_FPATH),
+      .INIT_FILE("/home/shc/projects/riscv-linux-boot/opensbi/build/platform/template/firmware/fw_dynamic.hex"), //(`RAM_FPATH),
       .USE_BOOTROM(0)
    ) main_memory8 (
       .clk_i   (clkwiz_o),
