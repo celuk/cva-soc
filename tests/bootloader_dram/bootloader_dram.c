6f45344cb8ad70e57a8818a7903e6d3da6e182dd
celuk <seyyid4091@gmail.com>
2025-08-19 23:42:39 +0300

    temporary fix for exception in atomics lrsc test

--- tests/bootloader_dram/bootloader_dram.c
+++ tests/bootloader_dram/bootloader_dram.c
@@ -22,7 +22,7 @@ static inline void jump_to_dram()
     asm volatile (
         //"lui   t0, 0x80000 \n"
         //"addi  t0, t0, 0x100 \n"
-        "li    t0, 0x80001000 \n"
+        "li    t0, 0x80000000 \n"
         "jalr  x0, t0, 0 \n"
     );
 }
@@ -84,7 +84,7 @@ int main()
     init();
 
     //update_trap_vector_base_address();
-    opensbi_init();
-    //jump_to_dram();
+    //opensbi_init();
+    jump_to_dram();
     return 0;
 }
