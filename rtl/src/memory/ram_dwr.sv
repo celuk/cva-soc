e71746f18f2fab5a0f59ffcec11559ccf64ee879
celuk <seyyid4091@gmail.com>
2025-09-17 13:47:46 +0300

    instable soft reset

--- rtl/src/memory/ram_dwr.sv
+++ rtl/src/memory/ram_dwr.sv
@@ -404,7 +404,7 @@ module ram32_dwr #(
           end
           SequenceDramWriteLengthCalc: begin
             dram_prog_ctr <= 32'h0;
-            dram_prog_addr <= 32'h0;
+            //dram_prog_addr <= 32'h0;
             if (prog_uart_do != ~0) begin
               dram_prog_size <= {dram_prog_size[3*8-1:0],prog_uart_do[7:0]};
               if (&instruction_byte_ctr) begin
@@ -481,7 +481,7 @@ module ram32_dwr #(
    assign rvalid_o = rvalid_r;
    assign rdata_o = ram_rdata;
    assign prog_mode_led_o = (state_prog == SequenceProgram);
-   assign system_reset_o = prog_sys_rst_n && dram_prog_sys_rst_n && !soft_rst && boot_done;
+   assign system_reset_o = prog_sys_rst_n && boot_done && (state_prog != SequenceDramWriteFinish);
    assign ram_prog_rd_en = (state_prog != SequenceFinish) && (state_prog != SequenceDramWriteFinish);
 
    // New DRAM output assignments
